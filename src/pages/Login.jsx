714f1ea7fe6864f55bdee6a9bcc1c65e9ecf2379
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-14 10:48:04 +0200

    Login 2

---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      localStorage.setItem('token', res.data.token);
+      navigate('/');
+    } catch (err) {
+      alert('Connexion échouée');
+    }
+  };
+}
+
+export default Login;

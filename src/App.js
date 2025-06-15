78e897e93668f4fa15223e340cd7df71113bb78b
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-15 19:16:24 +0200

    App 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/connexion" element={<Login />} />
         <Route path="/inscription" element={<Register />} />
+        <ToastContainer position="top-right" autoClose={3000} />
       </Routes>
     </Router>
   );

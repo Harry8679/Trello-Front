38b2333a0ac5a6cd66a11780a97c8ee61ceafec2
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-15 18:50:01 +0200

    Register 4

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', {
+      const res = await axios.post('http://localhost:3456/api/auth/register', {
         email,
         password,
         firstName,

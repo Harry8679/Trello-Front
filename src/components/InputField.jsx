947b5f177b4c3a17d2dc0a3a206232539488aef2
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-21 11:47:09 +0200

    Input Field 2

---
+++ src/components/InputField.jsx
@@ -0,0 +1,24 @@
+// src/components/InputField.jsx
+const InputField = ({
+  type = 'text',
+  placeholder,
+  value,
+  onChange,
+  required = false,
+  className = '',
+  ...rest
+}) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      required={required}
+      className={`w-full border p-2 mb-4 rounded ${className}`}
+      {...rest}
+    />
+  );
+};
+
+export default InputField;

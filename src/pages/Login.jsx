82f76744b052d7fcee2372c2a54b9537fb37fa82
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-21 12:11:50 +0200

    Useless comment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,10 +17,6 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
-      /* const res = await axios.post(`${API_URL}/api/auth/login`, { email, password });
-      localStorage.setItem('token', res.data.token);
-      navigate('/');*/
       const res = await axios.post(`${API_URL}/api/auth/login`, { email, password });
       setToken(res.data.token); // Zustand
       toast.success('Connexion réussie');

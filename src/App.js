f23f354335d0c27c4a1bc7f5180b046c440a482e
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-22 12:17:19 +0200

    Protected Route

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import ForgotPassword from './pages/ForgotPassword';
 import MyProjects from './pages/MyProjects';
+import PrivateLayout from './layouts/PrivateLayout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <Router>
       <ToastContainer position="top-right" autoClose={3000} />
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Home />} />
         <Route path="/connexion" element={<Login />} />
         <Route path="/inscription" element={<Register />} />
         <Route path="/mot-de-passe-oublie" element={<ForgotPassword />} />
-        <Route path='/mes-projets' element={<MyProjects />} />
+
+        {/* Private routes with navbar */}
+        <Route element={<PrivateLayout />}>
+          <Route path="/mes-projets" element={<MyProjects />} />
+        </Route>
       </Routes>
     </Router>
   );

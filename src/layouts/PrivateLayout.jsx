0e8b34608c152949ec603ab65afee1090de38d29
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-22 12:14:16 +0200

    Private Layout

---
+++ src/layouts/PrivateLayout.jsx
@@ -0,0 +1,19 @@
+// src/layouts/PrivateLayout.jsx
+import { Outlet, Navigate } from 'react-router-dom';
+import useAuthStore from '../store/useAuthStore';
+import Navbar from '../components/Navbar';
+
+const PrivateLayout = () => {
+  const { token } = useAuthStore();
+
+  if (!token) return <Navigate to="/connexion" />;
+
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+export default PrivateLayout;

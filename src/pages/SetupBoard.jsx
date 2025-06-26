bd5eb7d33dc31f31f1b222f78773dba59b62d677
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-26 13:13:05 +0200

    UseEffect removed

--- src/pages/SetupBoard.jsx
+++ src/pages/SetupBoard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import useAuthStore from '../store/useAuthStore';
